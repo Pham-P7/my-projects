b7c4275f9112a6a7800afb009efdca57a465635b
Phat Pham <parentpham07@gmail.com>
2022-03-23 00:22:00 -0500

    added more features to triplex project

--- c++ files/tripleX.cpp
+++ c++ files/tripleX.cpp
@@ -4,5 +4,17 @@ int main()
     std::cout << "You have a long quest ahead and you must pack food, water and supplies";
     std::cout << std::endl;
     std::cout << "You must bring the EXACT AMOUNT or it will be to heavy or you will die";
+
+    int a = 4;
+    int b = 3;
+    int c = 7;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
