f69acd25494415b4eeb950faf0e297533aa4b593
Phat Pham <parentpham07@gmail.com>
2022-03-23 00:52:15 -0500

    Hello World in C#

---
+++ C# files/Hello/Hello/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+windows C# coding
+HelloWorld Application
+Phat Pham
+3/23/22
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
