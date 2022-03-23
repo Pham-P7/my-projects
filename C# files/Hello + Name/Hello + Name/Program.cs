6e0087f5e4ce27fdc98071ddbc385fad7dfafdae
Phat Pham <parentpham07@gmail.com>
2022-03-23 01:01:09 -0500

    Hello + Name C#

---
+++ C# files/Hello + Name/Hello + Name/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Phat Pham
+Hello + Name
+3/23/22
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello___Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello " + Name);
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
