1d0fb852d322c28ea5060df79680242872a1ad0f
Phat Pham <parentpham07@gmail.com>
2022-03-22 22:47:54 -0500

    Update tripleX.cpp

--- c++ files/tripleX.cpp
+++ c++ files/tripleX.cpp
@@ -4,5 +4,5 @@ int main()
     std::cout << "You have a long quest ahead and you must pack food, water and supplies";
     std::cout << std::endl;
     std::cout << "You must bring the EXACT AMOUNT or it will be to heavy or you will die";
-    return 1;
+    return 0;
 }
