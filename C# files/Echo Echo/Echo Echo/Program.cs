19b7d614e7f89c64f37db4312c427bf17102d05b
Phat Pham <parentpham07@gmail.com>
2022-03-23 19:16:35 -0500

    Echo Echo file

---
+++ C# files/Echo Echo/Echo Echo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echo_Echo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello " + Name);
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
