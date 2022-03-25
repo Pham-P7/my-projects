f6cbbf2c421b25fd016283fdbcf284c4486cc860
Phat Pham <parentpham07@gmail.com>
2022-03-25 13:51:40 -0500

    Update number generator.py

--- python files/number generator.py
+++ python files/number generator.py
@@ -1,21 +1,33 @@
 import random
+def placeFinder():
+    global AOrder
+    global BOrder
+    global COrder
+    print(f"the sum of these 3 numbers is {a+b+c}")
+    print(f"The product of these 3 numbers is {a*b*c}")
+    if a > b and a > c:
+        AOrder = "first"
+    elif (a > b and a < c) or (a > c and a < b):
+        AOrder = "middle"
+    else:
+        AOrder = "last"
+    if b > a and b > c:
+        BOrder = "first"
+    elif (b > a and b < c) or (b > c and b < a):
+        BOrder = "middle"
+    else:
+        BOrder = "last"
+    if c > a and c > b:
+        COrder = "first"
+    elif (c > a and c < b) or (c > b and c < a):
+        COrder = "middle"
+    else:
+        COrder = "last"
 level = 1
 if level == 1:
     a = random.randint(1,5)
     b = random.randint(1,5)
     c = random.randint(1,5)
     print(a,b,c)
-    print(f"the sum of these 3 numbers is {a+b+c}")
-    print(f"The product of these 3 numbers is {a*b*c}")
-    if a > b and a > c:
-        AOrder = "greatest"
-    elif (a > b and a < c) or (a > c and a < b):
-        AOrder = "middle"
-    else:
-        AOrder = "least"
-    if b > a and b > c:
-        BOrder = "greatest"
-    elif (b > a and b < c) or (B)
-    if c > a and c > b:
-        Corder = "greatest"
+
     list = [a,b,c]
