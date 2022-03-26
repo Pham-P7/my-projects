3336bb57612dc99aca3af17077f909ec47b28f54
Phat Pham <parentpham07@gmail.com>
2022-03-26 03:19:20 -0500

    updated Hello World C#

--- C# files/Hello World/Hello World/HelloForm.Designer.cs
+++ C# files/Hello World/Hello World/HelloForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.MyImageButton = new System.Windows.Forms.Button();
+            this.CheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +42,36 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Hello, World!";
             // 
+            // MyImageButton
+            // 
+            this.MyImageButton.Location = new System.Drawing.Point(340, 259);
+            this.MyImageButton.Name = "MyImageButton";
+            this.MyImageButton.Size = new System.Drawing.Size(120, 126);
+            this.MyImageButton.TabIndex = 1;
+            this.MyImageButton.Text = "button1";
+            this.MyImageButton.UseVisualStyleBackColor = true;
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.AutoSize = true;
+            this.CheckBox.Location = new System.Drawing.Point(359, 132);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(101, 24);
+            this.CheckBox.TabIndex = 2;
+            this.CheckBox.Text = "checkBox1";
+            this.CheckBox.UseVisualStyleBackColor = true;
+            // 
             // HelloForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CheckBox);
+            this.Controls.Add(this.MyImageButton);
             this.Controls.Add(this.label1);
             this.Name = "HelloForm";
             this.Text = "Hello, World!";
+            this.Load += new System.EventHandler(this.HelloForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +80,7 @@
         #endregion
 
         private Label label1;
+        private Button MyImageButton;
+        private CheckBox CheckBox;
     }
 }
--- C# files/Hello World/Hello World/HelloForm.cs
+++ C# files/Hello World/Hello World/HelloForm.cs
@@ -1,3 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Hello_World
 {
     public partial class HelloForm : Form
@@ -5,6 +15,15 @@ namespace Hello_World
         public HelloForm()
         {
             InitializeComponent();
+        }
+
+        private void HelloForm_Load(object sender, EventArgs e)
+        {
+            CheckBox.Text = "GoodBye";
+            MyImageButton.Text = "";
+            MyImageButton.Image = Image.FromFile("arrow_button.png");
+            this.BackgroundImage = Image.FromFile("BlueBackground.jpg");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
         }
     }
 }
