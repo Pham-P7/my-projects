f5bd7fc5634f2249ca8946b41e209211174ff8ee
Phat Pham <parentpham07@gmail.com>
2022-03-31 18:55:15 -0500

    Update tripleX.cpp

--- c++ files/tripleX.cpp
+++ c++ files/tripleX.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 int main()
 {
+    // main story 
     std::cout << "You have a long quest ahead and you must pack food, water and supplies";
     std::cout << std::endl;
     std::cout << "You must bring the EXACT AMOUNT or it will be to heavy or you will die";
 
-    const int a = 4;
-    const int b = 3;
-    const int c = 7;
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 7;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int sum = CodeA + CodeB + CodeC;
+    const int product = CodeA * CodeB * CodeC;
     
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "There are 3 numbers in the password" << std::endl;
+    std::cout << "The sum of all 3 numbers = " << sum << std::endl;
+    std::cout << "The product of all 3 numbers = " << product << std::endl;
 
     return 0;
 }
