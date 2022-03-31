2f288f2029817f1d57f4b73d9bfdb3ab1209d1cc
Phat Pham <parentpham07@gmail.com>
2022-03-31 18:42:30 -0500

    updated const rule

--- c++ files/tripleX.cpp
+++ c++ files/tripleX.cpp
@@ -5,13 +5,13 @@ int main()
     std::cout << std::endl;
     std::cout << "You must bring the EXACT AMOUNT or it will be to heavy or you will die";
 
-    int a = 4;
-    int b = 3;
-    int c = 7;
+    const int a = 4;
+    const int b = 3;
+    const int c = 7;
 
-    int sum = a + b + c;
-    int product = a * b * c;
-
+    const int sum = a + b + c;
+    const int product = a * b * c;
+    
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
