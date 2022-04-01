e8dd00fd2b374935b23f5d644186313767659ed3
Phat Pham <parentpham07@gmail.com>
2022-04-01 13:56:07 -0500

    updated playerone files

--- C# files/Chapter 6/PlayerOne/PlayerOne/Form1.Designer.cs
+++ C# files/Chapter 6/PlayerOne/PlayerOne/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "enter your player name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(297, 111);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(201, 23);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(297, 182);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(201, 42);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "ENTER";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBoxName;
+        private Button StartButton;
     }
 }
