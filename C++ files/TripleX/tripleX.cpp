17c51d780d69590cad439472351346359338eb14
Phat Pham <parentpham07@gmail.com>
2022-04-11 13:14:03 -0500

    Chapter 6 updated on playerone

--- C++ files/TripleX/tripleX.cpp
+++ C++ files/TripleX/tripleX.cpp
@@ -11,20 +11,21 @@ void PrintIntroduction(int Difficulty)
 bool PlayGame(int Difficulty)
 {
     PrintIntroduction(Difficulty);
+    // difficulty rating 
     const int CodeA = rand() % Difficulty + Difficulty;
     const int CodeB = rand() % Difficulty + Difficulty;
     const int CodeC = rand() % Difficulty + Difficulty;
-    
+    // create the sum of the product 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
-    
+    //print the hints in the game
     std::cout << "There are 3 numbers in the password";
     std::cout << "\n The sum of all 3 numbers = " << CodeSum;
     std::cout << "\n The product of all 3 numbers = " << CodeProduct << "\n";
-
+    // create the input guesses 
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA >> GuessB >> GuessC;
-
+    // create the guess sum and product to compaire to code sum and product 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProuct = GuessA * GuessB * GuessC;
     
@@ -42,11 +43,13 @@ bool PlayGame(int Difficulty)
 
 int main()
 {
+    // time sets difficulty rating 
     srand(time(NULL));
     int LevelDifficulty = 1;
     const int MaxLevel = 7;
     while (LevelDifficulty <= MaxLevel)
     {
+        // 
         bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear();
         std::cin.ignore();
@@ -55,8 +58,6 @@ int main()
         {
             ++ LevelDifficulty;
         }
-        
-
     }
     std::cout << "\nWell done, The guilds quest  board is currently empty congrats";
     return 0;
