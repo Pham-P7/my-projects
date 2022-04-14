684f6acde7eac846661a72be98201b2d2c041bda
Phat Pham <parentpham07@gmail.com>
2022-04-14 13:39:23 -0500

    stopcode for c++ alarmclock

--- C++ files/AlarmClock/AlarmClock.cpp
+++ C++ files/AlarmClock/AlarmClock.cpp
@@ -1,20 +1,25 @@
-// this is my alarm clock because I can't get up in the morning
+// header statments 
 #include <iostream>
 #include <ctime>
 #include <Windows.h>
 #include <string>
 using namespace std;
+// main function sets off at 7 and keep keeps rechecking time 
 int main()
 {
+    // start stopcode and ask for a variable
+    string stopCode;
+    std::cin >> stopCode; 
     while(true)
     {
+        // checks the time until the
         time_t now = time(NULL);
         struct tm *tm_struct = localtime(&now);
         int hour = tm_struct->tm_hour;
-        if(hour == 7)
+        if(hour == 7 && stopCode != "go")
         {
             int c = 0;
-            while(c != 1000)
+            while(c != 1000 && stopCode != "go")
             {
                 Beep(523,500);
                 c++;
