1d71215daf3c92d16369971972cfec69669b1351
Phat Pham <parentpham07@gmail.com>
2022-04-16 01:31:30 -0500

    updated files

--- C++ files/Unreal Engine/BullCowGame/BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ C++ files/Unreal Engine/BullCowGame/BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,22 +4,49 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    SetupGame();
+    PrintLine(FString::Printf(TEXT("The hiddenword is: %s. \n It is %i characters long"), *HiddenWord, HiddenWord.Len()));
+    // or
+    // PrintLine(TEXT("The hiddenword is: %s"), *HiddenWord);
+
     PrintLine(TEXT("Welcome to the Bulls and Cows game!"));
-    PrintLine(TEXT("guess the four letter word!")); // remove the number later
+    PrintLine(TEXT("guess the %i letter word!"), HiddenWord.Len()); // remove the number later
     PrintLine(TEXT("Press enter to continue"));
-    HiddenWord = TEXT("cake");
+
+    // set lives
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    PrintLine(Input);
+
+    HiddenWord.Len();
+    Input.Len();
+
     if(Input == HiddenWord)
     {
         PrintLine(TEXT("You have won!"));
     }
     else
     {
+        if(HiddenWord.Len() != Input.Len())
+        {
+            PrintLine(TEXT("opps the word is %i characters long, try again"), HiddenWord.Len());
+        }
+
         PrintLine(TEXT("You have lose"));
     }
+    // check if isogram
+    //check if right amount of characters 
+    // remove life
+
+    // check if they want to play again
+    // if yes start again
+    // if no show word and quit game
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cake");
+    lives = 4;
 }
