ff1b81f27cedcf3e07d3c5115ae3b211ca50b75c
Phat Pham <parentpham07@gmail.com>
2022-04-18 13:59:19 -0500

    Update nitrogen stations.py

--- python files/nitrogen stations.py
+++ python files/nitrogen stations.py
@@ -2,7 +2,8 @@ from random import randint
 station = ""
 i = int(input("how many loops do you wish to test?"))
 c = 1
+visted = []
 while c != i:
     firstRoll = randint(1,10)
     if(firstRoll == 1 or station == "GroundWater"):
-        
+        visted.append("ground water")
