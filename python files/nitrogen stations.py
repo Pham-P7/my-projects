8deae4177fe7bac448e25930cfaa56ed221869c4
Phat Pham <parentpham07@gmail.com>
2022-04-20 13:58:34 -0500

    Update nitrogen stations.py

--- python files/nitrogen stations.py
+++ python files/nitrogen stations.py
@@ -7,3 +7,17 @@ while c != i:
     firstRoll = randint(1,10)
     if(firstRoll == 1 or station == "GroundWater"):
         visted.append("ground water")
+        dice = randint(1,6) 
+        if dice in [1,3,5]:
+            station = "Surface Water"
+        elif dice in [2,4,6]:
+            station = "ocean"
+        continue
+    if(firstRoll == 2 or station == "Ocean"):
+        visted.append("ocean")
+        dice = randint(1,6)
+        if dice in [1]:
+            station = "GroundWater"
+        elif dice in [2,3]:
+            station = "LivePlant"
+        elif 
