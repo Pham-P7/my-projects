f90db9d6942ccb0bd871927aee826850bfad676c
Phat Pham <parentpham07@gmail.com>
2022-05-09 23:15:40 -0500

    Update Pokemon shell.cpp

--- C++ files/TrevorFiles/TrevorPractice/Pokemon shell.cpp
+++ C++ files/TrevorFiles/TrevorPractice/Pokemon shell.cpp
@@ -5,23 +5,30 @@ created by Phat Pham
 #include <iostream>
 #include <string>
 using namespace std;
+class Pokemon
+{
+    public:
+    string name;
+    string type;
+    int attack;
+    int health;
+    int armor;    
+};
+class Attack
+{
+    public:
+    int damage;
+};
 void Introduction()
 {
     string Name;
     std::cout << "what is your name?\n";
     std::cin >> Name;
-    std::cout << ("\nHello %s, welcome to the world of pokemon\nThis however is a copy of the game so you get stuck with one battle\n", Name);
+    std::cout << ("\nHello %s, welcome to the world of pokemon\nThis however is a copy of the game made in a few hours so don't expect much\n", Name);
 }
 int PickPokemon()
 {
-    while(true)
-    std::cout << "what will your first pokemon be?\n" << "(a) Charmander, (b) squrt gun, or (c) usless plant thing";
-    string FirstPick;
-    std::cin >> FirstPick;
-    if(FirstPick == "a")
-    {
-        return;
-    }
+
 }
 bool Battle()
 {
