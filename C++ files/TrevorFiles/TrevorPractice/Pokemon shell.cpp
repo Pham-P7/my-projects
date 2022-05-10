7cd2119f148df1681d9a0d63534c606179fae66e
Phat Pham <parentpham07@gmail.com>
2022-05-09 23:33:05 -0500

    new settings

--- C++ files/TrevorFiles/TrevorPractice/Pokemon shell.cpp
+++ C++ files/TrevorFiles/TrevorPractice/Pokemon shell.cpp
@@ -8,23 +8,22 @@ using namespace std;
 class Pokemon
 {
     public:
-    string name;
-    string type;
-    int attack;
-    int health;
-    int armor;    
+    string name, type;
+    int attack, health, armor;
 };
 class Attack
 {
     public:
-    int damage;
+    int attackMult, attackPlus;
+    string statEffect;
 };
 void Introduction()
 {
     string Name;
     std::cout << "what is your name?\n";
     std::cin >> Name;
-    std::cout << ("\nHello %s, welcome to the world of pokemon\nThis however is a copy of the game made in a few hours so don't expect much\n", Name);
+    std::cout << ("\nHello %s, welcome to the world of pokemon\n",Name);
+    std::cout << "This is a bad copy of pokemon made in a few hours so don't expect much";
 }
 int PickPokemon()
 {
