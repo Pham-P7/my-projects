08b6ceca5978bf45f14a8870518ee5bd6f23de8d
Phat Pham <parentpham07@gmail.com>
2022-05-09 23:48:48 -0500

    moved into file

--- C++ files/TrevorFiles/TrevorPractice/Pokemon/Pokemon shell.cpp
+++ C++ files/TrevorFiles/TrevorPractice/Pokemon/Pokemon shell.cpp
@@ -4,15 +4,7 @@ created by Phat Pham
 */
 #include <iostream>
 #include <string>
-#include <fstream>
 using namespace std;
-
-std::ifstream input( "filename.txt" );
-
-for( std::string line; getline( input, line ); )
-{
-//do what you want for each line input here
-}
 
 class Pokemon
 {
