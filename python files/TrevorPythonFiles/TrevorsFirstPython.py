444ef529abedbb26cf05d49917267a492c3bec41
Phat Pham <parentpham07@gmail.com>
2022-05-18 13:21:27 -0500

    Update TrevorsFirstPython.py

--- python files/TrevorPythonFiles/TrevorsFirstPython.py
+++ python files/TrevorPythonFiles/TrevorsFirstPython.py
@@ -2,10 +2,16 @@
 #many ways for same outcome
 # function is not needed in python
 print("Hello, World")
+print("Hello, " + "World")
 # we don't use "int" or "string" to say what type of data the variable will be
 x = "Hello, World"
+#special formating ways to print a variable
 print(x)
+print("%s" %(x))
+print(f"{x}")
+
 x = "World"
+#concatination of strings plus variable
 print("Hello,", x)
 print("Hello, " + x)
 print(f"Hello, {x}")
