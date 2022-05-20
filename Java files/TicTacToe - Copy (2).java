87f13ef01d37dd660a58c458855734a8e39848b4
Phat Pham <parentpham07@gmail.com>
2022-05-19 21:45:58 -0500

    new files

---
+++ Java files/TicTacToe - Copy (2).java
@@ -0,0 +1,140 @@
+import javax.swing.*; import java.awt.*; import java.awt.event.*;import java.awt.event.ActionEvent;import java.awt.event.ActionListener;
+
+  public class TicTacToe extends JFrame implements ActionListener
+  {
+    JFrame frame = new JFrame();
+    JPanel titlePanel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean playerturn = true;
+    String Winner;
+    String line = null;
+    String Fill = null;
+    ActionEvent cl;
+      public String checkWinner() {
+        for (int a = 0; a < 8; a++) {
+            switch (a) {
+            case 0:
+                line = buttons[0].getText() + buttons[1].getText() + buttons[2].getText();
+                break;
+            case 1:
+                line = buttons[3].getText() + buttons[4].getText() + buttons[5].getText();
+                break;
+            case 2:
+                line = buttons[6].getText() + buttons[7].getText() + buttons[8].getText();
+                break;
+            case 3:
+                line = buttons[0].getText() + buttons[3].getText() + buttons[6].getText();
+                break;
+            case 4:
+                line = buttons[1].getText() + buttons[4].getText() + buttons[7].getText();
+                break;
+            case 5:
+                line = buttons[2].getText() + buttons[5].getText() + buttons[8].getText();
+                break;
+            case 6:
+                line = buttons[0].getText() + buttons[4].getText() + buttons[8].getText();
+                break;
+            case 7:
+                line = buttons[2].getText() + buttons[4].getText() + buttons[6].getText();
+                break;
+            }
+              if (line.equals("XXX")) {
+                  Fill = "X";
+                  break;
+              } 
+              else if (line.equals("OOO")) {
+                  Fill = "O";
+                  break;
+              }
+            }
+            return Fill;
+        }
+    public void TicTacToe()
+    {
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(800,800);
+      frame.getContentPane().setBackground(new Color(255,255,255));
+      frame.setLayout(new BorderLayout());
+      frame.setVisible(true);
+  
+      textfield.setBackground(new Color(50,50,50));
+      textfield.setForeground(new Color(25, 225, 0));
+      textfield.setFont(new Font("Comic Sans MS", Font.BOLD,75));
+      textfield.setHorizontalAlignment(JLabel.CENTER);
+      textfield.setText("Tic-Tac-Toe");
+      textfield.setOpaque(true);
+  
+      titlePanel.setLayout(new BorderLayout());
+      titlePanel.setBounds(0,0,800,150);
+  
+      button_panel.setLayout(new GridLayout(3,3));
+      button_panel.setBackground(new Color(0,0,0));
+  
+      for(int i = 0; i < 9; i++)
+      {
+        buttons[i] = new JButton();
+        buttons[i].setText(" ");
+        button_panel.add(buttons[i]);
+        buttons[i].setFont(new Font("Comic Sans MS",Font.BOLD,100));
+        buttons[i].setFocusable(false);
+        buttons[i].addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            for(int i = 0; i < 9; i++)
+            {
+                if(e.getSource() == buttons[i])
+                {
+                  if(playerturn)
+                  {
+                    if(buttons[i].getText() == "")
+                    {
+                        buttons[i].setForeground(new Color(255,0,0));
+                        buttons[i].setText("X");
+                        playerturn = false;
+                        textfield.setText("O turn");
+                        Winner = checkWinner();
+                        if(Winner.equals("X")){
+                          textfield.setText("X won");
+                          break;
+                        }
+        
+                    }
+                  }
+                  else
+                    {
+                        if(buttons[i].getText() == "")
+                        {
+                            buttons[i].setForeground(new Color(0,0,255));
+                            buttons[i].setText("O");
+                            playerturn = true;
+                            textfield.setText("X turn");
+                            Winner = checkWinner();
+                            if(Winner.equals("O")){
+                              textfield.setText("O won");
+                              break;
+                            }
+                        }
+                    }
+                }
+            }
+            if((!(Winner.equals("X"))) && (!(Winner.equals("O")))){
+              textfield.setText("Tie");
+            for(int i = 0; i < 9; i++){
+              buttons[i].setEnabled(false);
+              } 
+            }
+          }
+        });
+      }
+  
+      titlePanel.add(textfield);
+      frame.add(titlePanel,BorderLayout.NORTH);
+      frame.add(button_panel);
+    }
+
+public static void main(String[] args){
+  TicTacToe tictactoe = new TicTacToe();
+  actionPerformed(cl);
+}
+} 
--- Java files/TicTacToe.java
+++ Java files/TicTacToe.java
@@ -1,154 +1,138 @@
-import javax.swing.*; import java.awt.*; import java.awt.event.*; import java.util.*;
+import java.awt.GridLayout;
 
-  public class TicTacToe extends JFrame implements ActionListener
-  {
-    Random random = new Random();
-    JFrame frame = new JFrame();
-    JPanel titlePanel = new JPanel();
-    JPanel button_panel = new JPanel();
-    JLabel textfield = new JLabel();
-    JButton[] buttons = new JButton[9];
-    boolean playerturn;
-    String Winner;
-    String line = null;
-    String Fill = null;
-    public void firstTurn(){
-      for(int i = 0; i < 9; i++)
-      {
-        buttons[i].setText("");
-      }
-      if(random.nextInt(2) == 0)
-      {
-        textfield.setText("X turn");
-        playerturn = true;
-      }
-      else
-      {
-        textfield.setText("O turn");
-        playerturn = false;
-      }
-    }
-      public String checkWinner() {
-        for (int a = 0; a < 8; a++) {
-            switch (a) {
-            case 0:
-                line = buttons[0].getText() + buttons[1].getText() + buttons[2].getText();
-                break;
-            case 1:
-                line = buttons[3].getText() + buttons[4].getText() + buttons[5].getText();
-                break;
-            case 2:
-                line = buttons[6].getText() + buttons[7].getText() + buttons[8].getText();
-                break;
-            case 3:
-                line = buttons[0].getText() + buttons[3].getText() + buttons[6].getText();
-                break;
-            case 4:
-                line = buttons[1].getText() + buttons[4].getText() + buttons[7].getText();
-                break;
-            case 5:
-                line = buttons[2].getText() + buttons[5].getText() + buttons[8].getText();
-                break;
-            case 6:
-                line = buttons[0].getText() + buttons[4].getText() + buttons[8].getText();
-                break;
-            case 7:
-                line = buttons[2].getText() + buttons[4].getText() + buttons[6].getText();
-                break;
-            }
-              if (line.equals("XXX")) {
-                  Fill = "X";
-                  break;
-              } 
-              else if (line.equals("OOO")) {
-                  Fill = "O";
-                  break;
-              }
-            }
-            return Fill;
-        }
-    public void tictactoe()
-    {
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(800,800);
-      frame.getContentPane().setBackground(new Color(255,255,255));
-      frame.setLayout(new BorderLayout());
-      frame.setVisible(true);
-  
-      textfield.setBackground(new Color(50,50,50));
-      textfield.setForeground(new Color(25, 225, 0));
-      textfield.setFont(new Font("Comic Sans MS", Font.BOLD,75));
-      textfield.setHorizontalAlignment(JLabel.CENTER);
-      textfield.setText("Tic-Tac-Toe");
-      textfield.setOpaque(true);
-  
-      titlePanel.setLayout(new BorderLayout());
-      titlePanel.setBounds(0,0,800,150);
-  
-      button_panel.setLayout(new GridLayout(3,3));
-      button_panel.setBackground(new Color(0,0,0));
-  
-      for(int i = 0; i < 9; i++)
-      {
-        buttons[i] = new JButton();
-        button_panel.add(buttons[i]);
-        buttons[i].setFont(new Font("Comic Sans MS",Font.BOLD,100));
-        buttons[i].setFocusable(false);
-        buttons[i].addActionListener(this);
-      }
-  
-      titlePanel.add(textfield);
-      frame.add(titlePanel,BorderLayout.NORTH);
-      frame.add(button_panel);
-    }
-    public void actionPerformed(ActionEvent e)
-    {
-      for(int i = 0; i < 9; i++)
-    {
-        if(e.getSource() == buttons[i])
-        {
-          if(playerturn)
-          {
-            if(buttons[i].getText() == "")
-            {
-                buttons[i].setForeground(new Color(255,0,0));
-                buttons[i].setText("X");
-                playerturn = false;
-                textfield.setText("O turn");
-                Winner = checkWinner();
-                if(Winner.equals("X")){
-                  textfield.setText("X won");
-                  break;
-                }
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-            }
-          }
-          else
-            {
-                if(buttons[i].getText() == "")
-                {
-                    buttons[i].setForeground(new Color(0,0,255));
-                    buttons[i].setText("O");
-                    playerturn = true;
-                    textfield.setText("X turn");
-                    Winner = checkWinner();
-                    if(Winner.equals("O")){
-                      textfield.setText("O won");
-                      break;
-                    }
-                }
-            }
+
+public class TicTacToe extends JPanel {
+
+    char playerSign = 'x';
+    int totalCells = 9;
+    int totalRows = 3;
+    int totalColumns = 3;
+    JButton[] jButtons = new JButton[totalCells];
+
+    public TicTacToe() {
+        GridLayout ticTacToeGridLayout = new GridLayout(totalRows, totalColumns);
+        setLayout(ticTacToeGridLayout);
+
+        createButtons();
+
+    }
+
+    public void createButtons() {
+        for (int i = 0; i <= 8; i++) {
+
+            jButtons[i] = new JButton();
+
+            jButtons[i].setText("");
+
+            jButtons[i].addActionListener(e -> {
+
+                JButton clickedBtn = (JButton) e.getSource();
+                clickedBtn.setText(String.valueOf(playerSign));
+                clickedBtn.setEnabled(false);
+
+
+                if (playerSign == 'x')
+                    playerSign = 'o';
+                else
+                    playerSign = 'x';
+
+                showWinner();
+            });
+
+            add(jButtons[i]);
         }
     }
-    if((!(Winner.equals("X"))) && (!(Winner.equals("O")))){
-      textfield.setText("Tie");
-    for(int i = 0; i < 9; i++){
-      buttons[i].setEnabled(false);
-      } 
-    }
-  }
 
-public static void main(String[] args){
-  tictactoe();
+
+    public void showWinner() {
+
+        if (checkForWinner()) {
+
+            if (playerSign == 'x') playerSign = 'o';
+            else playerSign = 'x';
+
+            JOptionPane jOptionPane = new JOptionPane();
+            int dialog = JOptionPane.showConfirmDialog(jOptionPane, "Game Over. " + "The winner is " + playerSign + " ", "Result",
+                    JOptionPane.DEFAULT_OPTION);
+
+            if (dialog == JOptionPane.OK_OPTION)
+                System.exit(0);
+
+
+        } else if (checkIfMatchDraw()) {
+            JOptionPane jOptionPane = new JOptionPane();
+            int dialog = JOptionPane.showConfirmDialog(jOptionPane, "Game Draw", "Result", JOptionPane.DEFAULT_OPTION);
+
+            if (dialog == JOptionPane.OK_OPTION)
+                System.exit(0);
+        }
+    }
+
+
+    public boolean checkIfMatchDraw() {
+        boolean gridsFull = true;
+        for (int i = 0; i < totalCells; i++) {
+            if (jButtons[i].getText().equals("")) {
+                gridsFull = false;
+            }
+        }
+        return gridsFull;
+    }
+
+    public boolean checkForWinner() {
+        return checkAllRows() || checkAllColumns() || checkTheDiagonals();
+    }
+
+    public boolean checkAllRows() {
+        int i = 0;
+        for (int j = 0; j < 3; j++) {
+            if (jButtons[i].getText().equals(jButtons[i + 1].getText()) && jButtons[i].getText().equals(jButtons[i + 2].getText())
+                    && !jButtons[i].getText().equals("")) {
+                return true;
+            }
+            i = i + 3;
+
+        }
+        return false;
+    }
+
+
+    public boolean checkAllColumns() {
+
+        int i = 0;
+        for (int j = 0; j < 3; j++) {
+
+            if (jButtons[i].getText().equals(jButtons[i + 3].getText()) && jButtons[i].getText().equals(jButtons[i + 6].getText())
+                    && !jButtons[i].getText().equals("")) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public boolean checkTheDiagonals() {
+        if (jButtons[0].getText().equals(jButtons[4].getText()) && jButtons[0].getText().equals(jButtons[8].getText())
+                && !jButtons[0].getText().equals(""))
+            return true;
+        else
+            return jButtons[2].getText().equals(jButtons[4].getText()) && jButtons[2].getText().equals(jButtons[6].getText())
+                    && !jButtons[2].getText().equals("");
+    }
+
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Tic Tac Toe Game");
+
+        jFrame.getContentPane().add(new TicTacToe());
+        jFrame.setBounds(500, 500, 600, 550);
+        jFrame.setVisible(true);
+        jFrame.setLocationRelativeTo(null);
+    }
+
 }
-} 
