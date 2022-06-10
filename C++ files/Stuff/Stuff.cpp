65f139c74bb253437c51fb8aff0700309f53d209
Phat Pham <parentpham07@gmail.com>
2022-06-10 14:02:41 -0500

    jk

---
+++ C++ files/Stuff/Stuff.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+string
+double
+float
+int
+bool
+
+*/
+int main(){
+    for(int i = 0; i <= 100; i++)
+    {
+        std::cout << i << "\n";
+    }
+}
---
+++ python files/print.py
@@ -0,0 +1,4 @@
+b = [1,"Hi",3,4.0,True,6]
+print(b)
+b.append(9)
+print(b)
