dc8ed1d9509e3ce44d3cbae841861a9a4e3bbc3d
Phat Pham <parentpham07@gmail.com>
2022-06-15 11:23:05 -0500

    Update Yahtzee.py

--- python files/Yahtzee.py
+++ python files/Yahtzee.py
@@ -16,14 +16,14 @@ while(YN == "y" or YN == "Y"):
     for i in range(0,6):
         if rolls[i] == 1:
             Ace += 1
-        if rolls[i] == 2:
+        elif rolls[i] == 2:
             Two += 2
-        if rolls[i] == 3:
+        elif rolls[i] == 3:
             Three += 3
-        if rolls[i] == 4:
+        elif rolls[i] == 4:
             Four += 4
-        if rolls[i] == 5:
+        elif rolls[i] == 5:
             Five += 5
-        if rolls[i] == 6:
+        elif rolls[i] == 6:
             Six += 6
     
