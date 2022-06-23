75b0c00f0f042a775f7501c028bfb21a2560c87a
Phat Pham <parentpham07@gmail.com>
2022-06-23 11:05:57 -0500

    Update TrevorsFirstPython.py

--- python files/TrevorPythonFiles/TrevorsFirstPython.py
+++ python files/TrevorPythonFiles/TrevorsFirstPython.py
@@ -1,22 +1,22 @@
 # we use no header files because it already knows what "print" is
-#many ways for same outcome
-# function is not needed in python
+# many ways for same outcome
+# main function is not needed in python
 print("Hello, World")
 print("Hello, \
 World")
 print("Hello, " + "World")
 # we don't use "int" or "string" to say what type of data the variable will be
 x = "Hello, World"
-#special formating ways to print a variable
+# special formating ways to print a variable
 print(x)
 print("%s" %(x))
 print(f"{x}")
 
 y = "Hello, "
 x = "World"
-#concatination of strings plus variable
+# concatination of strings plus variable
 print(y  + x)
-#below are strings plus variable and we can flip it to work for y and not x
+# below are strings plus variable and we can flip it to work for y and not x
 print("Hello,", x)
 print("Hello, " + x)
 print(f"Hello, {x}")
