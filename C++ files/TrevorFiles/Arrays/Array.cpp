0bf71365c7908cb27b7b02069a09be856678e52d
Phat Pham <parentpham07@gmail.com>
2022-06-27 12:16:52 -0500

    Update Array.cpp

--- C++ files/TrevorFiles/Arrays/Array.cpp
+++ C++ files/TrevorFiles/Arrays/Array.cpp
@@ -6,9 +6,10 @@ int main()
 {
   array<int,3> myarray {9,19,29};
 
-  for (int i=0; i<myarray.size(); ++i)
+  for (int i=0; i<myarray.size(); ++i){
     ++myarray[i];
-
-  for (int elem : myarray)
+  }
+  for (int elem : myarray){
     cout << elem << '\n';
+  }
 }
