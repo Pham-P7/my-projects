dcd2359e898361924e35658964382df97334033f
Phat Pham <parentpham07@gmail.com>
2022-06-27 12:15:57 -0500

    Update InArray.cpp

--- C++ files/TrevorFiles/Arrays/InArray.cpp
+++ C++ files/TrevorFiles/Arrays/InArray.cpp
@@ -5,9 +5,12 @@ int main()
 {
   int myarray[3] = {9,19,29};
 
-  for (int i=0; i<3; ++i)
+  for (int i=0; i<3; ++i){
     ++myarray[i];
+  }
 
-  for (int elem : myarray)
+  for (int elem : myarray){
     cout << elem << '\n';
+  }
+
 }
